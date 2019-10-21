dc8491e47bf4e3cc20146db6505593f39958cda2
Rajavel <rj11.devops@gmail.com>
2019-10-22 00:26:48 +0530

    Updated drive location

--- src/test/FirstSelenium.java
+++ src/test/FirstSelenium.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstSelenium {
 	public static void main(String[] args) {  
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Rajavel\\workspace\\chromedriver.exe");        
+		String projectLocation = System.getProperty("user.dir");
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\Rajavel\\workspace\\SeleniumTestcase\\lib\\chromeDriver\\chromedriver.exe");        
+		System.setProperty("webdriver.chrome.driver",projectLocation+"lib\\chromeDriver\\chromedriver.exe");
 	    WebDriver driver = new ChromeDriver();  
 	    driver.navigate().to("http://www.javatpoint.com/");
 	    driver.quit();
