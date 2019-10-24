a2533475cad23e0d5cf7e09f56aa4ead3d6bb209
rj11devops <56843110+rj11devops@users.noreply.github.com>
2019-10-24 08:38:30 +0530

    Update FirstSelenium.java

--- src/test/FirstSelenium.java
+++ src/test/FirstSelenium.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstSelenium {
 	public static void main(String[] args) {  
-		String projectLocation = System.getProperty("user.dir");
-		//System.setProperty("webdriver.chrome.driver","C:\\Users\\Rajavel\\workspace\\SeleniumTestcase\\lib\\chromeDriver\\chromedriver.exe");        
-		System.setProperty("webdriver.chrome.driver",projectLocation+"lib\\chromeDriver\\chromedriver.exe");
+		//String projectLocation = System.getProperty("user.dir");
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\Rajavel\\workspace\\SeleniumTestcase\\src\\test\\chromedriver.exe");        
+		System.setProperty("webdriver.chrome.driver","D:\\a\\1\\a\\src\\test\\chromedriver.exe");
 	    WebDriver driver = new ChromeDriver();  
 	    driver.navigate().to("http://www.javatpoint.com/");
 	    driver.quit();
